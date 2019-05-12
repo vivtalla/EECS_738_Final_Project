9ba7333690ad7e211270e1159ce60755ba3a9edf
Vivek Tallavajhala <vivtalla@ku.edu>
2019-05-12 16:11:36 -0500

    add ten teams data

---
+++ Python/dataget.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sportsreference.ncaab.teams import Teams
+
+dataset = pd.DataFrame()
+teams = Teams()
+print("Total Teams:",len(teams))
+totalTeams = len(teams)
+n = 0
+for team in teams:
+    dataset = pd.concat([dataset, team.schedule.dataframe_extended])
+    n += 1
+    print(n, "of", totalTeams, "Done")
+    if n >= 10:
+        break
+dataset.to_csv('data-full.csv', encoding='utf-8', index=False)
