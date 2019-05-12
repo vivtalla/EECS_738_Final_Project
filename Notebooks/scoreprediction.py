5e3001384c604934db86235194ff9bb5aceab54a
Andre Kurait <AndreKurait@gmail.com>
2019-05-12 14:13:30 -0500

    data scrape

--- Notebooks/scoreprediction.py
+++ Notebooks/scoreprediction.py
@@ -10,19 +10,26 @@ FIELDS_TO_DROP = ['away_points', 'home_points', 'date', 'location',
 dataset = pd.DataFrame()
 print('Database Retrieving')
 teams = Teams()
+n = 1
 for team in teams:
     dataset = pd.concat([dataset, team.schedule.dataframe_extended])
-print('Database Retrieving Finished')
-X = dataset.drop(FIELDS_TO_DROP, 1).dropna().drop_duplicates()
-y = dataset[['home_points', 'away_points']].values
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-parameters = {'bootstrap': False,
-              'min_samples_leaf': 3,
-              'n_estimators': 50,
-              'min_samples_split': 10,
-              'max_features': 'sqrt',
-              'max_depth': 6}
-print('Training Model')
-model = RandomForestRegressor(**parameters)
-model.fit(X_train, y_train)
-print(model.predict(X_test).astype(int), y_test)
+    # if n > 1:
+    #     break
+    # n += 1
+    print("Team", n, "Done")
+dataset.to_csv('data-full.csv', encoding='utf-8', index=False)
+
+# print('Database Retrieving Finished')
+# X = dataset.drop(FIELDS_TO_DROP, 1).dropna().drop_duplicates()
+# y = dataset[['home_points', 'away_points']].values
+# X_train, X_test, y_train, y_test = train_test_split(X, y)
+# parameters = {'bootstrap': False,
+#               'min_samples_leaf': 3,
+#               'n_estimators': 50,
+#               'min_samples_split': 10,
+#               'max_features': 'sqrt',
+#               'max_depth': 6}
+# print('Training Model')
+# model = RandomForestRegressor(**parameters)
+# model.fit(X_train, y_train)
+# print(model.predict(X_test).astype(int), y_test)
